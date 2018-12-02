53eba1119f780e1bd20f7cd03850f2d15d03b348
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-12-02 02:17:50 -0800

    added components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import en from "@angular/common/locales/en";
+import { HttpClientModule } from "@angular/common/http";
+import { NgZorroAntdModule, NZ_I18N, en_US } from "ng-zorro-antd";
+import { registerLocaleData } from "@angular/common";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//Ng Bootstrap
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+//Components
+import { AppComponent } from "./app.component";
+import { JumbotronComponent } from "./jumbotron/jumbotron.component";
+import { SearchFieldComponent } from "./search-field/search-field.component";
+import { SearchResultComponent } from "./search-result/search-result.component";
+//Shared Components
+import { ButtonComponent } from "./shared/button/button.component";
+import { InputComponent } from "./shared/input/input.component";
+import { TableLayoutComponent } from "./shared/table-layout/table-layout.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JumbotronComponent,
+    ButtonComponent,
+    InputComponent,
+    SearchFieldComponent,
+    TableLayoutComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
