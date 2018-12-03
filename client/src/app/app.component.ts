a202ec2ea5a0a76235ff5860a67885080f1a9cc4
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-12-03 03:07:27 -0800

    minor reorg of UI components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, NgModule } from "@angular/core";
+//TODO: import input, button, and table here
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
+  //TODO: Add directives for input, button and table
 })
 export class AppComponent {
+  //TODO: Declare data as object
+
   title = "client";
+
+  //TODO: Add on submit function that takes the value from the input, and insert as params in the get method
+  //Subscribe (aka promise) { response => data}
 }
--- client/src/app/search-field/search-field.component.ts
+++ client/src/app/search-field/search-field.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-search-field',
-  templateUrl: './search-field.component.html',
-  styleUrls: ['./search-field.component.css']
+  selector: "app-search-field",
+  templateUrl: "./search-field.component.html",
+  styleUrls: ["./search-field.component.css"]
 })
 export class SearchFieldComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  title = "search";
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      search: ""
+    });
   }
+  ngOnInit() {}
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
